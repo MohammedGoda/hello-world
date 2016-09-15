4586d243b553004c2cd8b9c309c1f84913e6570e
MohammedGoda <MohammedGoda@users.noreply.github.com>
2016-09-15 19:48:33 +0200

    Add files via upload

---
+++ create.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class create : MonoBehaviour {
+
+    public GameObject lastObj;
+    
+    // Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Debug.Log(lastObj.gameObject.name);
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            lastObj = (GameObject)Instantiate(lastObj, EditedTransform(lastObj), Quaternion.Euler(0, 0, 0));
+        }
+        else if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            //lastObj = (GameObject)Instantiate(lastObj, EditedTransform(lastObj), lastObj.transform.rotation * Quaternion.Euler(0, 90, 0));
+            lastObj = (GameObject)Instantiate(lastObj, EditedTransform(lastObj), Quaternion.Euler(0, 90, 0));
+        }
+        else if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            //lastObj = (GameObject)Instantiate(lastObj, EditedTransform(lastObj), lastObj.transform.rotation * Quaternion.Euler(0, -90, 0));
+            lastObj = (GameObject)Instantiate(lastObj, EditedTransform(lastObj), Quaternion.Euler(0, -90, 0));
+        }
+        else if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            lastObj = (GameObject)Instantiate(lastObj, EditedTransform(lastObj), Quaternion.Euler(0, 180, 0));
+        }
+
+        /*if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            lastObj = (GameObject) Instantiate(lastObj, lastObj.transform.position + new Vector3(0, 0, 2), Quaternion.identity);
+        }
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            lastObj = (GameObject)Instantiate(lastObj, lastObj.transform.position + new Vector3(0.5f, 0, 1.5f), Quaternion.identity * Quaternion.Euler(0, 90, 0));
+        }
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            lastObj = (GameObject)Instantiate(lastObj, lastObj.transform.position + new Vector3(-0.5f, 0, 1.5f), Quaternion.identity * Quaternion.Euler(0, -90, 0));
+        }
+        */
+
+    }
+
+    Vector3 EditedTransform(GameObject obj)
+    {
+        Vector3 editedPos = obj.transform.position + (1f * obj.transform.forward);
+        return editedPos;
+    }
+
+}
---
+++ shipCtrl.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class shipCtrl : MonoBehaviour {
+
+    public Rigidbody rb;
+    private float speed;
+    private float rotateSpeed;
+
+    // Use this for initialization
+    void Start () {
+        rb = gameObject.GetComponent<Rigidbody>();
+        speed = 0.1f;
+        rotateSpeed = 0;
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            rotateSpeed = 0.2f;
+        }
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            rotateSpeed = -0.2f;
+        }
+
+    }
+
+    void FixedUpdate()
+    {
+        rb.MovePosition(transform.position + speed * transform.forward.normalized);
+        rb.MoveRotation(transform.rotation * Quaternion.Euler(0, rotateSpeed, 0));
+        rotateSpeed = 0;
+
+    }
+}
